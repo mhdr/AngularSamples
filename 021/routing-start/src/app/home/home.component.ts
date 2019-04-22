c8d56cebaf83d4b47f416cf1116c78ec406fb01a
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2019-04-22 10:01:12 +0430

    021

--- 021/routing-start/src/app/home/home.component.ts
+++ 021/routing-start/src/app/home/home.component.ts
@@ -14,8 +14,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
-    // do some complex calculation
-    this.router.navigate(['/servers']);
+  onLoadServer(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
   }
 }

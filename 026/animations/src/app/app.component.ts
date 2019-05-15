a8e3e642208bec98926a614e508c2fdd9218d3f9
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2019-05-15 18:16:14 +0430

    026

--- 026/animations/src/app/app.component.ts
+++ 026/animations/src/app/app.component.ts
@@ -14,9 +14,6 @@ import {bounceIn} from 'ng-animate';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('animation1', [transition('1 <=> 2', useAnimation(bounceIn, {
-      params: {timing: 2, delay: 0.05}
-    }))]),
     trigger('preventAnimation', [
       // use an empty animation as parent of the list1 to prevent animation for it
       transition('void => *', [])
@@ -34,37 +31,26 @@ import {bounceIn} from 'ng-animate';
           opacity: 0,
           transform: 'translateX(-100px)'
         }),
-        animate(300)])
+        animate(300)]),
+      // animation for removing item from DOM
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
     ])
   ],
 })
 export class AppComponent {
   title = 'animations';
-  state1 = 1;
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
     this.list.push(item);
   }
 
-  onAnimate() {
-    // switch state
-    if (this.state1 === 1) {
-      this.state1 = 2;
-    } else {
-      this.state1 = 1;
-    }
-  }
-
-  onAnimationStarted(event) {
-    console.log(event);
-  }
-
-  onAnimationDone(event) {
-    console.log(event);
-  }
-
   onDelete(item: string) {
-
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }

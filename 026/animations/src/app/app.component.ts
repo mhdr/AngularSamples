c5178c7da87e05a1aaee7988093108ed9212f306
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2019-05-15 13:49:43 +0430

    026

--- 026/animations/src/app/app.component.ts
+++ 026/animations/src/app/app.component.ts
@@ -16,12 +16,36 @@ import {bounceIn} from 'ng-animate';
   animations: [
     trigger('animation1', [transition('1 <=> 2', useAnimation(bounceIn, {
       params: {timing: 2, delay: 0.05}
-    }))])
+    }))]),
+    trigger('preventAnimation', [
+      // use an empty animation as parent of the list1 to prevent animation for it
+      transition('void => *', [])
+    ]),
+    trigger('list1', [state('in', style({
+      // this is the final style that the animation goes to it
+      opacity: 1,
+      transform: 'translateX(0px)'
+    })),
+      // void is for items not present in the DOM
+      transition('void => *', [
+        style({
+          // this style will be applied before animation starts
+          // so this would be the initial style of element
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)])
+    ])
   ],
 })
 export class AppComponent {
   title = 'animations';
   state1 = 1;
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
 
   onAnimate() {
     // switch state
@@ -38,5 +62,9 @@ export class AppComponent {
 
   onAnimationDone(event) {
     console.log(event);
+  }
+
+  onDelete(item: string) {
+
   }
 }
